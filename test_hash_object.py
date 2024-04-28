a6e2d59197a444ad77bec0bbe25190d145e3d649
yasuda ryuuya <ysdzm75@gmail.com>
2024-04-28 14:21:58 +0900

    コードをきれいに

--- test_hash_object.py
+++ test_hash_object.py
@@ -1,22 +1,23 @@
-import sys
 import subprocess
-import os
 
 def test_hash_object():
-    # hello.txtを作成
+    # テスト用のファイルを作成
     with open("hello.txt", "w") as f:
         f.write("Hello, world!")
     with open("hoge.txt", "w") as f:
         f.write("hogehoge")
 
+    # テストケース1: -wオプションなしでファイル1つを指定
     output = subprocess.check_output(["python", "git.py", "hash-object", "hello.txt"], universal_newlines=True).strip()
     git_hash_output = subprocess.check_output(["git", "hash-object", "./hello.txt"], universal_newlines=True).strip()
     assert output == git_hash_output, "Output mismatch: without the -w option, the output does not match the output of the git command"
 
+    # テストケース2: -wオプションありでファイル1つを指定
     output = subprocess.check_output(["python", "git.py", "hash-object", "-w", "hello.txt"], universal_newlines=True).strip()
     git_hash_output = subprocess.check_output(["git", "hash-object", "-w", "./hello.txt"], universal_newlines=True).strip()
     assert output == git_hash_output, "Output mismatch: with the -w option, the output does not match the output of the git command"
 
+    # テストケース3: -wオプションなしで複数のファイルを指定
     output = subprocess.check_output(["python", "git.py", "hash-object", "hello.txt", "hoge.txt"], universal_newlines=True).strip()
     git_hash_output = subprocess.check_output(["git", "hash-object", "./hello.txt", "./hoge.txt"], universal_newlines=True).strip()
     assert output == git_hash_output, "Output mismatch: without the -w option, the output does not match the output of the git command"
